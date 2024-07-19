a55989b8bbb220de69a2e4c63dbb901a1fda5a3a
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-19 09:16:31 +0200

    feat: remove item & clear items functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById('clear');
 
 
 // Functions
@@ -45,6 +46,24 @@ function createIcon(classes) {
     return icon;
 }
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        if (confirm("Are you sure?")) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearItems() {
+    if (confirm("Are you sure?")) {
+        while (itemList.firstChild) {
+            itemList.removeChild(itemList.firstChild);
+        }
+    }
+}
+
 
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
