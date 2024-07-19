5ab70a71f5494b0ce105b0a347eeed8a85157f95
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-19 12:59:43 +0200

    feat: edit & update functionality created

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const itemFilter = document.getElementById("filter");
 const clearBtn = document.getElementById("clear");
+const formBtn = document.getElementById("form-btn");
+
+let isEditMode = false;
 
 // Functions
 
 function checkUI() {
+  itemInput.value = "";
+
   const items = itemList.querySelectorAll("li");
 
   if (items.length === 0) {
@@ -16,6 +21,11 @@ function checkUI() {
     clearBtn.style.display = "block";
     itemFilter.style.display = "block";
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.background = '#333';
+
+  isEditMode = false;
 }
 
 function onAddItemSubmit(e) {
@@ -29,7 +39,15 @@ function onAddItemSubmit(e) {
     return;
   }
 
-  itemInput.value = "";
+  // Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  }
 
   //   Create Item DOM Element
   addItemToDOM(newItem);
@@ -38,6 +56,8 @@ function onAddItemSubmit(e) {
   addItemToStorage(newItem);
 
   checkUI();
+
+  itemInput.value = "";
 }
 
 function addItemToDOM(item) {
@@ -148,7 +168,21 @@ function displayItems() {
 function onClickItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
     removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemToEdit(e.target);
   }
+}
+
+function setItemToEdit(item) {
+  isEditMode = true;
+
+  itemList.querySelectorAll('li').forEach((i) => i.classList.remove('edit-mode'));
+
+  item.classList.add('edit-mode');
+  
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>   Update Item';
+  formBtn.style.backgroundColor = '#228B22';
+  itemInput.value = item.textContent;
 }
 
 function init() {
